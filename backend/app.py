e9aaedd61412c4767a64d8ee15580f62dda1d8ac
01Subham <subhamthakur1758@gmail.com>
2026-05-17 23:49:15 +0530

    "UPDATED Backend> app.py"

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 
@@ -46,5 +47,12 @@ app.register_blueprint(
     url_prefix="/api/negotiations"
 )
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
